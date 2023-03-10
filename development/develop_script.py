5cef32a6344392bd4a384b9dab75722aad58e8b3
Thomas Vergauwen <thomas.vergauwen@meteo.be>
2023-03-10 13:44:01 +0100

    fix

--- development/develop_script.py
+++ development/develop_script.py
@@ -6,7 +6,9 @@ Created on Thu Oct  6 13:25:02 2022
 """
 
 # import vlinder_toolkit
-import os, sys
+import vlinder_toolkit
+import os
+import sys
 import pandas as pd
 import numpy as np
 from pathlib import Path
@@ -16,23 +18,21 @@ lib_folder = Path(__file__).resolve().parents[1]
 sys.path.append(str(lib_folder))
 
 
-import vlinder_toolkit
-
 #%% % Import
 
-testdatafile = os.path.join(str(lib_folder), 'tests', 'test_data',  'testdata_okt_small.csv')
+testdatafile = os.path.join(
+    str(lib_folder), 'tests', 'test_data',  'testdata_okt_small.csv')
 
-static_data =  os.path.join(str(lib_folder), 'static_data', 'vlinder_metadata.csv')
-
-
+static_data = os.path.join(
+    str(lib_folder), 'static_data', 'vlinder_metadata.csv')
 
 
 # #% Setup dataset
 settings = vlinder_toolkit.Settings()
 settings.update_settings(input_data_file=testdatafile,
-                          input_metadata_file=static_data,
-                          output_folder='/home/thoverga/Documents/VLINDER_github/vlinder_toolkit'
-                          )
+                         input_metadata_file=static_data,
+                         output_folder='/home/thoverga/Documents/VLINDER_github/vlinder_toolkit'
+                         )
 
 
 dataset = vlinder_toolkit.Dataset()
