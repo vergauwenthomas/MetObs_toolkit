5c94a869b600d79da2cd063bcbce90fe57aea039
Thomas Vergauwen <thomas.vergauwen@meteo.be>
2023-03-21 15:51:21 +0100

    WIP on template mapper in PyQt5

---
+++ GUI/main.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 20 09:47:48 2023
+
+@author: thoverga
+"""
+
+# command: designer in terminal to open the desinger tool
+
+
+
+import sys
+from pathlib import Path
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog, QMainWindow
+from PyQt5.uic import loadUi
+
+import data_func
+import template_func
+
+
+
+
+
+
+class MainWindow(QDialog):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        # Load gui widgets
+        loadUi('templ_build.ui', self) #open the ui file
+        
+        # Setup error message dialog
+        self.error_dialog = QtWidgets.QErrorMessage(self)
+        
+        
+        # Callbacks (Getters)
+        self.Browse_data_B.clicked.connect(lambda: self.browsefiles_data()) #browse datafile
+        self.Browse_metadata_B.clicked.connect(lambda: self.browsefiles_metadata()) #browse metadatafile
+
+        # self.start_mapping_B.clicked.connect(lambda: self.read_datafiles())
+        # debug
+        self.start_mapping_B.clicked.connect(lambda: self.set_templ_map_val())
+        default_path = '/home/thoverga/Documents/VLINDER_github/vlinder_toolkit/tests/test_data/vlinderdata_small.csv'
+        self.data_file_T.setText(default_path)
+
+
+# =============================================================================
+# Helpers
+# =============================================================================
+    def get_val(self, data_func_return):
+
+        if not data_func_return[1]:
+            self.error_dialog.showMessage(data_func_return[2])
+        return data_func_return[0]
+
+
+# =============================================================================
+# Init values
+# =============================================================================
+    def set_templ_map_val(self):
+        print(self.data_file_T.text())
+        # First try reading the datafile
+        data_columns = self.read_datafiles(self.data_file_T.text())
+        
+        
+        # Check if meta data is given, if so read the metadata columns
+        if len(self.metadata_file_T.text()) > 2:
+            metadata_columns = self.read_datafiles(self.data_file_T.text())
+        else:
+            metadata_columns = []
+        
+        # Set defaults appropriate
+        template_func.set_templ_vals(self, data_columns, metadata_columns)
+        
+
+# =============================================================================
+# Triggers
+# =============================================================================
+    def browsefiles_data(self):
+        fname=QFileDialog.getOpenFileName(self, 'Select data file', str(Path.home()))
+        self.data_file_T.setText(fname[0]) #update text
+        
+
+    def browsefiles_metadata(self):
+        fname=QFileDialog.getOpenFileName(self, 'Select metadata file', str(Path.home()))
+        self.metadata_file_T.setText(fname[0]) #update text
+
+    def read_datafiles(self, filepath):
+        _return = data_func.get_columns(filepath=filepath)
+        print(_return)
+        columns = self.get_val(_return)
+        
+        return columns
+
+
+
+
+
+
+# =============================================================================
+# Main and protector
+# =============================================================================
+
+def main():
+    mainwindow = MainWindow()
+
+    widget = QtWidgets.QStackedWidget()
+    widget.addWidget(mainwindow)
+    # widget.show()
+    
+    return widget
+    
+    
+    
+    
+    
+if __name__ == '__main__':
+    app=QApplication(sys.argv)
+    
+    widget = main()
+    widget.show()
+    sys.exit(app.exec_())
+
+
+
---
+++ GUI/template_func.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 20 13:00:45 2023
+
+@author: thoverga
+"""
+
+
+Obs_map_values = {
+    
+     'temp':{'units': ['K', 'Celcius'], 'description':'2mT', 'dtype':'object'},
+     'radiation_temp':{'units': ['K', 'Celcius'], 'description':'Blackglobe temperature', 'dtype':'float64'},
+     'humidity':{'units': ['%'], 'description':'Relative humidity', 'dtype':'float64'},
+     'precip':{'units': ['l/hour x m²'], 'description':'Precipitation intensity', 'dtype':'float64'},     
+     'precip_sum':{'units': ['l/m²', 'Celcius'], 'description':'Precipitation cumulated', 'dtype':'float64'},
+     'wind_speed':{'units': ['m/s'], 'description':'Average wind speed', 'dtype':'float64'},
+     'wind_gust':{'units': ['m/s'], 'description':'Wind gust', 'dtype':'float64'},
+     'wind_direction':{'units': ['°'], 'description':'Wind direction (from north CW)', 'dtype':'float64'},
+     'pressure':{'units': ['Pa'], 'description':'Air pressure', 'dtype':'float64'},
+     'pressure_at_sea_level':{'units': ['Pa'], 'description':'Pressure at sealevel', 'dtype':'float64'}
+     
+    }
+
+Dt_map_values = {
+    'datetime': {'format':'%Y-%m-%d %H:%M:%S', 'dtype': object},
+    '_date':{'format':'%Y-%m-%d', 'dtype': object},
+    '_time':{'format':'%H:%M:%S', 'dtype': object},
+    }
+
+Meta_map_values = {
+    'name': {'description': 'Station name/ID', 'dtype':'object'},
+    'lat':{'description': 'Latitude', 'dtype':'float64'},
+    'lon':{'description': 'Longitude', 'dtype':'float64'},
+    'location':{'description': 'Location identifier', 'dtype':'object'},
+    'call_name':{'description': 'Pseudo name of station', 'dtype':'object'},
+    'network':{'description': 'Name of the network', 'dtype':'object'}
+    }
+
+
+
+def set_templ_vals(main, data_columns, metadata_columns):
+    enable_all_boxes(main)
+    csv_columns = data_columns
+    csv_columns.insert(0, 'NO MAPPING')
+    
+    
+    # Get all obs boxes for colum mapping
+    obs_boxes = [main.temp_col_CB, main.radtemp_col_CB, main.hum_col_CB,
+                 main.pre_col_CB, main.pre_s_col_CB, main.wind_col_CB,
+                 main.gust_col_CB, main.dir_col_CB, main.p_col_CB,
+                 main.psea_col_CB, main.datetime_col_CB, main.date_col_CB, 
+                 main.time_col_CB]
+    
+    for box in obs_boxes:
+        box.setEnabled(True)
+        box.addItems(csv_columns)
+        
+    metadata_boxes = [main.name_col_CB, main.lat_col_CB, main.lon_col_CB,
+                      main.loc_col_CB, main.call_col_CB, main.network_col_CB]
+    
+    # combine all possible columns:
+    csv_columns.extend(metadata_columns)
+    for box in metadata_boxes:
+        box.setEnabled(True)
+        box.addItems(csv_columns)
+    
+    
+    # =============================================================================
+    # Observation types       
+    # =============================================================================
+    
+    
+    
+    # Fill with defaults
+    main.temp_units_CB.addItems(Obs_map_values['temp']['units'])
+    main.temp_desc_T.setText(Obs_map_values['temp']['description'])
+    
+    main.radtemp_units_CB.addItems(Obs_map_values['radiation_temp']['units'])
+    main.radtemp_desc_T.setText(Obs_map_values['radiation_temp']['description'])
+    
+    main.hum_units_CB.addItems(Obs_map_values['humidity']['units'])
+    main.hum_desc_T.setText(Obs_map_values['humidity']['description'])
+
+    main.pre_units_CB.addItems(Obs_map_values['precip']['units'])
+    main.pre_desc_T.setText(Obs_map_values['precip']['description'])
+
+    main.pre_s_units_CB.addItems(Obs_map_values['precip_sum']['units'])
+    main.pre_s_desc_T.setText(Obs_map_values['precip_sum']['description'])
+    
+    main.wind_units_CB.addItems(Obs_map_values['wind_speed']['units'])
+    main.wind_desc_T.setText(Obs_map_values['wind_speed']['description'])
+    
+    main.gust_units_CB.addItems(Obs_map_values['wind_gust']['units'])
+    main.gust_desc_T.setText(Obs_map_values['wind_gust']['description'])
+    
+    main.dir_units_CB.addItems(Obs_map_values['wind_direction']['units'])
+    main.dir_desc_T.setText(Obs_map_values['wind_direction']['description'])
+    
+    main.p_units_CB.addItems(Obs_map_values['pressure']['units'])
+    main.p_desc_T.setText(Obs_map_values['pressure']['description'])
+    
+    main.psea_units_CB.addItems(Obs_map_values['pressure_at_sea_level']['units'])
+    main.psea_desc_T.setText(Obs_map_values['pressure_at_sea_level']['description'])
+    
+    # =============================================================================
+    #     Datetime
+    # =============================================================================
+     
+    # Fill with defaults
+    main.datetime_fmt_T.setText(Dt_map_values['datetime']['format'])
+    main.date_fmt_T.setText(Dt_map_values['_date']['format'])
+    main.time_fmt_T.setText(Dt_map_values['_time']['format'])
+    
+    
+def enable_all_boxes(main):
+    main.temp_col_CB.setEnabled(True)
+    main.temp_units_CB.setEnabled(True)
+    
+    main.radtemp_units_CB.setEnabled(True)
+    main.radtemp_desc_T.setEnabled(True)
+    main.hum_units_CB.setEnabled(True)
+    main.hum_desc_T.setEnabled(True)
+
+    main.pre_units_CB.setEnabled(True)
+    main.pre_desc_T.setEnabled(True)
+
+    main.pre_s_units_CB.setEnabled(True)
+    main.pre_s_desc_T.setEnabled(True)
+    
+    main.wind_units_CB.setEnabled(True)
+    main.wind_desc_T.setEnabled(True)
+    
+    main.gust_units_CB.setEnabled(True)
+    main.gust_desc_T.setEnabled(True)
+    
+    main.dir_units_CB.setEnabled(True)
+    main.dir_desc_T.setEnabled(True)
+    
+    main.p_units_CB.setEnabled(True)
+    main.p_desc_T.setEnabled(True)
+    
+    main.psea_units_CB.setEnabled(True)
+    main.psea_desc_T.setEnabled(True)
+   
