409d0fc67f0e116640731a40ee90e48791fe1df3
Thomas Vergauwen <thomas.vergauwen@meteo.be>
2023-03-28 16:45:12 +0200

    analysis class

---
+++ vlinder_toolkit/analysis.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 28 15:35:07 2023
+
+@author: thoverga
+"""
+from datetime import datetime
+
+from vlinder_toolkit.dataset import Dataset
+from vlinder_toolkit.df_helpers import (init_multiindexdf,
+                                        datetime_subsetting)
+
+class Analysis(Datset):
+    def __init__(self, obsdf, metadf, settings):
+        self.df = obsdf
+        self.metadf = metadf
+        self.settings = settings
+
+
+
+    # =============================================================================
+    #     Setters
+    # =============================================================================
+
+
+     def subset_period(self, startdt, enddt):
+        if not isinstance(startdt, type(datetime)):
+            print(f' {startdt} not a datetime type. Ignore subsetting!')
+            return
+        if not isinstance(enddt, type(datetime)):
+            print(f' {enddt} not a datetime type. Ignore subsetting!')
+            return
+
+        self.df = datetime_subsetting(self.df, startdt, enddt)
+
+    # =============================================================================
+    #   Helpers
+    # =============================================================================
+    def _subset_stations(self, stationslist):
+        df = self.df.loc[self.df.index.get_level_values(
+                    'name').isin(stationslist)]
+        return df
+
+
+    # =============================================================================
+    #   Analyse method
+    # =============================================================================
+    def make_diurnal_cycle(self, stations=None, obstype='temp', relative=False, refstation=None):
+
+        # filter to stations and obstype
+        if isinstance(stations, str)
+
