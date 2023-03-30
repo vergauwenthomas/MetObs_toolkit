b9519224850c670d8ccd8704b2fb2b2bff9ac2e4
Thomas Vergauwen <thomas.vergauwen@meteo.be>
2023-03-30 16:04:27 +0200

    setup for codechange on templates

--- development/develop_script.py
+++ development/develop_script.py
@@ -53,22 +53,6 @@ dataset.import_data_from_file(coarsen_timeres=False)
 
 #%%
 
-obs = {'temp': {'map_column': 'Temperatuur', 'map_unit': 'Celcius', 'map_desc': '2mT'}, 'radiation_temp': {'map_column': np.nan, 'map_unit': np.nan, 'map_desc': np.nan}, 'humidity': {'map_column': 'Vochtigheid', 'map_unit': '%', 'map_desc': 'Relative humidity'}, 'precip': {'map_column': np.nan, 'map_unit': np.nan, 'map_desc': np.nan}, 'precip_sum': {'map_column': np.nan, 'map_unit': np.nan, 'map_desc': np.nan}, 'wind_speed': {'map_column': np.nan, 'map_unit': np.nan, 'map_desc': np.nan}, 'wind_gust': {'map_column': np.nan, 'map_unit': np.nan, 'map_desc': np.nan}, 'wind_direction': {'map_column': np.nan, 'map_unit': np.nan, 'map_desc': np.nan}, 'pressure': {'map_column': np.nan, 'map_unit': np.nan, 'map_desc': np.nan}, 'pressure_at_sea_level': {'map_column': np.nan, 'map_unit': np.nan, 'map_desc': np.nan}}
-
-dt = {'datetime': {'map_column': np.nan, 'map_fmt': np.nan}, 'date': {'map_column': 'Datum', 'map_fmt': '%Y-%m-%d'}, 'time': {'map_column': 'Tijd (UTC)', 'map_fmt': '%H:%M:%S'}}
-
-meta = {'name': {'map_column': 'Vlinder'}, 'lat': {'map_column': 'Windrichting'}, 'lon': {'map_column': 'Rukwind'}, 'location': {'map_column': np.nan}, 'call_name': {'map_column': np.nan}, 'network': {'map_column': np.nan}}
-
-#%%
-
-df = pd.DataFrame(obs).transpose()
-df.index.name = 'varname'
-df = df.dropna(axis = 0, how = 'all')
-df = df.reset_index()
-df = df.rename(columns={'map_column': 'template column name',
-                        'map_unit': 'units',
-                        'map_desc': 'description'})
-df['dtype'] = 'float64'
 
 
 
