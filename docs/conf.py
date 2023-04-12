bbaac15da2b0c62c87123c14cab938599fa72b38
Thomas Vergauwen <thomas.vergauwen@meteo.be>
2023-04-12 13:34:04 +0200

    second try

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ lib_folder = Path(__file__).resolve().parents[1]
 tlk_folder = os.path.join(str(lib_folder), 'vlinder_toolkit')
 
 sys.path.append(tlk_folder)
-# import vlinder_toolkit
+import vlinder_toolkit
 
 # #Add modules for automatic documentation
 
