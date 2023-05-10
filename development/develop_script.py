387185da012d194dde901e3519905a5b6e3ea050
Thomas Vergauwen <thomas.vergauwen@meteo.be>
2023-05-10 09:26:24 +0200

    solved pointer to settings problem

--- development/develop_script.py
+++ development/develop_script.py
@@ -41,13 +41,39 @@ dataset.update_settings(input_data_file=testdatafile,
                         output_folder='/home/thoverga/Documents'
                         )
 
+obstype = 'temp'
+print('before: ', dataset.settings.qc['qc_check_settings']["gross_value"][obstype]['min_value'])
+
+dataset.qc_settings_update(obstype=obstype, gross_value_min_value=-100)
 
 
-dataset.import_data_from_file()
-dataset.apply_quality_control(gross_value=True,
-                              persistance=False)
+print('after: ', dataset.settings.qc['qc_check_settings']["gross_value"][obstype]['min_value'])
 
 
-dataset.get_qc_stats('temp', make_plot=True)
+#%% Datset2
+
+
+dataset2 = metobs_toolkit.Dataset()
+dataset2.update_settings(input_data_file=testdatafile,
+                        # input_metadata_file=static_data,
+                        data_template_file= template,
+                        output_folder='/home/thoverga/Documents'
+                        )
+
+print('before2 : ', dataset2.settings.qc['qc_check_settings']["gross_value"][obstype]['min_value'])
+
+dataset2.qc_settings_update(obstype=obstype, gross_value_min_value=-100)
+
+
+print('after2: ', dataset2.settings.qc['qc_check_settings']["gross_value"][obstype]['min_value'])
+
+
+
 
 #%%
+
+from metobs_toolkit.settings import Settings
+
+testset = Settings()
+print('after settings creation: ', testset.qc['qc_check_settings']["gross_value"][obstype]['min_value'])
+
