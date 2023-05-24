3b082a2806524d5c8046e328d95902e4e65ab20f
Thomas Vergauwen <thomas.vergauwen@meteo.be>
2023-05-24 16:46:04 +0200

    cleanup

--- tests/push_test/IO_test.py
+++ tests/push_test/IO_test.py
@@ -11,12 +11,10 @@ import sys, os
 from pathlib import Path
 
 
-lib_folder = Path(__file__).resolve().parents[2]
-sys.path.insert(0, str(lib_folder))
 
 
 import metobs_toolkit
-print(metobs_toolkit.__version__)
+# print(metobs_toolkit.__version__)
 
 
 # %% import data from file (long standard format)
