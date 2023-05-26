8e52cefbb8fe456abe19dbcf1ce6d1e88e730d95
Thomas Vergauwen <82087298+vergauwenthomas@users.noreply.github.com>
2023-05-26 10:50:10 +0200

    Env fix for pyqt version inconsistencies (#206)
    * rebuild the conda yml, now using as mutch conda as possible. PyQt problems with spyder are resolved
    
    * new fix
    
    * cleanup

--- development/develop_script.py
+++ development/develop_script.py
@@ -20,31 +20,32 @@ sys.path.insert(0,str(lib_folder))
 
 import metobs_toolkit
 
-
+metobs_toolkit.launch_gui()
 
 #%%
 # data Ian
 # datafile = '/home/thoverga/Documents/VLINDER_github/MetObs_toolkit/tests/test_data/WBGTdata_totaal.csv'
 
-# data Wout
-datafile = '/home/thoverga/Documents/VLINDER_github/MetObs_toolkit/tests/test_data/Outdoor_module_Netatmo_Sara_new.csv'
-metafile ='/home/thoverga/Documents/VLINDER_github/MetObs_toolkit/tests/test_data/metadata_Outdoor_module_Netatmo_Sara_new.csv'
+# # data Wout
+# datafile = '/home/thoverga/Documents/VLINDER_github/MetObs_toolkit/tests/test_data/Outdoor_module_Netatmo_Sara_new.csv'
+# metafile ='/home/thoverga/Documents/VLINDER_github/MetObs_toolkit/tests/test_data/metadata_Outdoor_module_Netatmo_Sara_new.csv'
 
 
 
-# metobs_toolkit.build_template_prompt()
+# # metobs_toolkit.build_template_prompt()
 
 
 
 
-# Make an empty dataset
-dataset = metobs_toolkit.Dataset()
+# # Make an empty dataset
+# dataset = metobs_toolkit.Dataset()
 
-# Add the demo data files to the dataset settings
-dataset.update_settings(input_data_file = metobs_toolkit.demo_datafile,
-                        input_metadata_file = metobs_toolkit.demo_metadatafile,
-                        data_template_file = metobs_toolkit.demo_template,
-                        metadata_template_file = metobs_toolkit.demo_template, # Contains also the metadata mapping
-                        output_folder = '/home/thoverga/Documents/VLINDER_github/MetObs_toolkit/development'
-                        )
+# # Add the demo data files to the dataset settings
+# dataset.update_settings(input_data_file = metobs_toolkit.demo_datafile,
+#                         input_metadata_file = metobs_toolkit.demo_metadatafile,
+#                         data_template_file = metobs_toolkit.demo_template,
+#                         metadata_template_file = metobs_toolkit.demo_template, # Contains also the metadata mapping
+#                         output_folder = '/home/thoverga/Documents/VLINDER_github/MetObs_toolkit/development'
+#                         )
+
 
