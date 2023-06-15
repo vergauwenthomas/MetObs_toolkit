e687e12e3876a5a23369ad8e3d5b02ae5dd065a6
Thomas Vergauwen <thomas.vergauwen@meteo.be>
2023-06-15 14:18:30 +0200

    reproduction of problem

--- development/develop_script.py
+++ development/develop_script.py
@@ -26,67 +26,29 @@ import metobs_toolkit
 
 # # data
 # era5_congo_file = '/home/thoverga/Downloads/era5_data_kongo.csv'
-# data_file = '/home/thoverga/Documents/VLINDER_github/MetObs_toolkit/tests/test_data/testdata_testday/Kobe/meteo_soil_clean_2023-01-19.csv'
+data_file = '/home/thoverga/Documents/VLINDER_github/MetObs_toolkit/tests/test_data/testdata_testday/Ian/ATHTS01_all.csv'
 # metadata_file = '/home/thoverga/Documents/VLINDER_github/MetObs_toolkit/tests/test_data/testdata_testday/Kobe/CONGO_meta.csv'
-# template_file ='/home/thoverga/Documents/VLINDER_github/MetObs_toolkit/tests/test_data/testdata_testday/Kobe/CONGO_template.csv'
+template_file ='/home/thoverga/Documents/VLINDER_github/MetObs_toolkit/tests/test_data/testdata_testday/Ian/template.csv'
 #%%
 
 dataset = metobs_toolkit.Dataset()
 
 dataset.update_settings(output_folder=None,
-                        input_data_file=metobs_toolkit.demo_datafile,
-                        input_metadata_file=metobs_toolkit.demo_metadatafile,
-                        data_template_file=metobs_toolkit.demo_template,
-                        metadata_template_file=metobs_toolkit.demo_template)
+                        input_data_file=data_file,
+                        # input_metadata_file=metobs_toolkit.demo_metadatafile,
+                        data_template_file=template_file,
+                        # metadata_template_file=metobs_toolkit.demo_template,
+                        )
 
 
 dataset.import_data_from_file()
 
-dataset.coarsen_time_resolution()
+# dataset.coarsen_time_resolution()
 
-dataset.apply_quality_control()
-
-
-dataset.update_gaps_and_missing_from_outliers(n_gapsize=10)
+# dataset.apply_quality_control()
 
 
-
-#%%
-
-
-erafile = "/home/thoverga/Downloads/era5_data_3.csv"
-
-model = metobs_toolkit.Modeldata('era')
-# dataset.get_modeldata()
-
-
-model.set_model_from_csv(erafile)
-
-
-
-#%%
-for gap in dataset.gaps:
-    print(gap)
-
-
-#%%
-
-# sta = dataset.get_station('vlinder01')
-# sta.gaps[0].get_info()
-# sta.fill_gaps_automatic(model, max_interpolate_duration_str='10H', overwrite_fill=True)
-
-
-
-#%%
-# sta.fill_gaps_linear()
-# sta.fill_gaps_era5(modeldata=model)
-
-stanames = dataset.df.index.get_level_values('name').unique().to_list()
-
-for stan in stanames:
-    sta = dataset.get_station(stan)
-    print(f' {stan} has these gaps: {sta.gaps}')
-    sta.fill_gaps_automatic(model, max_interpolate_duration_str='10H')
+# dataset.update_gaps_and_missing_from_outliers(n_gapsize=10)
 
 
 
@@ -95,4 +57,26 @@ for stan in stanames:
 
 
 
+dataset.update_qc_settings(obstype='temp',
+                           step_max_decrease_per_sec=0.5,
+                           step_max_increase_per_sec=0.5)
 
+
+dataset.apply_quality_control(
+    obstype="temp",         # choose which observations you want to check
+    gross_value=False,       # set True if you want to perform the gross value check
+    persistance=False,       # set True if you want to perform the persistence check
+    step=True,              # set True if you want to perform the spike check
+    window_variation=False,  # set True if you want to perform the window variation check
+)
+
+
+# qc_statistics = dataset.get_qc_stats(
+#     obstype="temp",     # Specify which observation variable you want to get the statistics for; here we choose temperature
+#     stationname=None,   # None means all stations are plotted. You can also plot a specific station by saying 'station_A'
+#     make_plot=True,     # Set True to make a plot
+# )
+
+
+
+dataset.make_plot(colorby='label')
