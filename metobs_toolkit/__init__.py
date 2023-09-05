8e788029b8e7284b4aa68ea1c9d81c7418af6c04
Thomas Vergauwen <thomas.vergauwen@meteo.be>
2023-09-05 13:41:45 +0200

    update version

--- metobs_toolkit/__init__.py
+++ metobs_toolkit/__init__.py
@@ -84,5 +84,5 @@ from metobs_toolkit.dataset_settings_updater import Dataset
 # =============================================================================
 
 # DO not change this manually!
-__version__ = "0.1.1a2"
+__version__ = "0.1.2"
 
