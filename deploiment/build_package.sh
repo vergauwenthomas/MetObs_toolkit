31dc3deaa9eae7c5b0197cbda544ebee94b39afd
Thomas Vergauwen <thomas.vergauwen@meteo.be>
2024-09-02 16:29:25 +0200

    version fix

--- deploiment/build_package.sh
+++ deploiment/build_package.sh
@@ -38,11 +38,11 @@ poetry add poetry@latest --group dev
 poetry add pre-commit@latest --group dev
 
 # Toolkit documentation group
-poetry add myst_parser@latest --group documentation
+poetry add myst_parser@^3 --group documentation #4.0.x not comp with py3.9
 poetry add nbsphinx@latest --group documentation
 poetry add pandoc@latest --group documentation
 poetry add pydata-sphinx-theme@latest --group documentation
-poetry add sphinx@latest --group documentation
+poetry add sphinx@>= 7 --group documentation #v8.x.x not comp with py3.9
 poetry add sphinx-copybutton@latest --group documentation
 poetry add sphinx-rtd-theme@latest --group documentation
 
