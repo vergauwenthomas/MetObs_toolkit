b973e326d1e812d54845766b8ccb1a45d733586b
Thomas Vergauwen <thomas.vergauwen@meteo.be>
2025-04-14 09:22:23 +0200

    Chaining is a special case, so the comment might be confusing for all the non-chainers

--- metobs_toolkit/sensordata.py
+++ metobs_toolkit/sensordata.py
@@ -774,12 +774,14 @@ class SensorData:
         self, modeltimeseries, method="raw", overwrite_fill=False, method_kwargs={}
     ):
         for gap in self.gaps:
-            if not gap.flag_can_be_filled(overwrite_fill): # if flag_can_be_filled returns False, Gaps won't be filled
+            if not gap.flag_can_be_filled(
+                overwrite_fill
+            ):  # if flag_can_be_filled returns False, Gaps won't be filled
                 logger.warning(
                     f"{gap} cannot be filled because it already contains filled values, and overwrite fill is {overwrite_fill}."
                 )
                 continue
-            if overwrite_fill: # Inorder to chain methods, overwrite_fill must be False
+            if overwrite_fill:
                 # clear previous fill info
                 gap.flush_fill()
 
