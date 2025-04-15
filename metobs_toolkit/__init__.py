c85edccacfd10188f6858aa08d1b2e3c4257c0db
Thomas Vergauwen <thomas.vergauwen@meteo.be>
2025-04-15 09:15:20 +0200

    fix attempt in doc build + test install and version bump to 0.4.0a

--- metobs_toolkit/__init__.py
+++ metobs_toolkit/__init__.py
@@ -68,7 +68,7 @@ from metobs_toolkit.gee_api import connect_to_gee
 
 # DO not change this manually!
 
-__version__ = "0.3.1a"
+__version__ = "0.4.0a"
 
 
 # =============================================================================
