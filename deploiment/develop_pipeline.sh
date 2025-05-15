10976d4064cd1c8e1b939b66a178406fe4fc13fd
Thomas Vergauwen <thomas.vergauwen@meteo.be>
2025-05-15 09:49:02 +0200

    Remove deprecated deployment scripts and update project configuration for improved testing and documentation

--- deploiment/develop_pipeline.sh
+++ deploiment/develop_pipeline.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Run a full develop pipeline on the toolkit (update + build + doc + tests)
+# Run a full develop pipeline on the toolkit (update + build + doc + tests + code cleanup)
 
 echo " -----------------------------------"
 echo " ---- Running DEV pipeline ----"
@@ -9,32 +9,61 @@ echo " -----------------------------------"
 
 DEPLOY_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DEP_PIPE_LOG=${DEPLOY_DIR}/dev_pipeline_log
+REPODIR=${DEPLOY_DIR}/..
 
 cd ${DEPLOY_DIR}
 
 rm -f ${DEP_PIPE_LOG} #clean start
 touch ${DEP_PIPE_LOG}
 
-#1. Update and built package
-./build_package.sh 2>&1 | tee -a ${DEP_PIPE_LOG}
-
-
 #remove previous logs
 LOGDIR=${DEPLOY_DIR}/logs
 rm ${LOGDIR}/*_log
 
-#2. Run examples as test
-./run_examples_as_test.sh 2>&1 | tee -a ${DEP_PIPE_LOG}
 
-#3. Build documentation
-./build_documentation.sh 2>&1 | tee -a ${DEP_PIPE_LOG}
+#2. Update and built package
+echo "Updating the package and dependecies"
+rm ${REPODIR}/dist/*.whl
+rm ${REPODIR}/dist/*.tar.gz
+
+poetry update
+poetry show
+poetry build
+poetry install --all-extras --no-root
 
 
-#4. Run toolkit tests
-./run_tests.sh 2>&1 | tee -a ${DEP_PIPE_LOG}
+#1. Cleanup the code (run black, black config in pyproject file)
+echo "Running black"
+cd $REPODIR
+poetry run black . 2>&1 | tee -a ${DEP_PIPE_LOG}
 
-#5. Run Docstring tests
-./run_doctests.sh 2>&1 | tee -a ${DEP_PIPE_LOG}
+
+#3. Run tests
+echo "Running testing framework"
+cd ${REPODIR}/toolkit_tests
+poetry run pytest --mpl --mpl-generate-summary=html 2>&1 | tee -a ${DEP_PIPE_LOG}
+
+
+#4. Run notebook example as tests
+echo "Running testing framework"
+cd ${REPODIR}/docs/examples
+poetry run pytest --nbval-lax 2>&1 | tee -a ${DEP_PIPE_LOG}
+cd ${REPODIR}/docs/topics
+poetry run pytest --nbval-lax 2>&1 | tee -a ${DEP_PIPE_LOG}
+
+#5. Build documentation
+#./build_documentation.sh 2>&1 | tee -a ${DEP_PIPE_LOG}
+echo "Building documentation"
+cd ${REPODIR}
+cd docs
+poetry run ./build_doc 2>&1 | tee -a ${DEP_PIPE_LOG}
+
+
+
+echo "open logs in geany"
+geany ${DEP_PIPE_LOG} &
+
+
 
 echo " -----------------------------------"
 echo " ---- DONE RUNNING ----"
