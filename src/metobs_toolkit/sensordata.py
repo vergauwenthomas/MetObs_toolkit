5c685330e71869b09b0c164ab27914eec965bf90
Thomas Vergauwen <thomas.vergauwen@meteo.be>
2025-10-02 09:06:03 +0200

    functionality of pd_plot

--- src/metobs_toolkit/sensordata.py
+++ src/metobs_toolkit/sensordata.py
@@ -4,6 +4,7 @@ from typing import Literal, Union
 import copy
 import numpy as np
 import pandas as pd
+from matplotlib.pyplot.axes import Axes
 
 
 from metobs_toolkit.backend_collection.dev_collection import copy_doc
@@ -747,6 +748,51 @@ class SensorData:
     # ------------------------------------------
 
     # plots are defined on station and dataset level
+    
+    @log_entry
+    def pd_plot(self, show_labels: list=['ok'], **pdplotkwargs) -> Axes:
+        """
+        Create a pandas plot of the sensor data with optional label filtering.
+
+        Parameters
+        ----------
+        show_labels : list of str, optional
+            List of quality control labels to include in the plot. Records with 
+            other labels are converted to NaN to avoid interpolation artifacts.
+            Default is ['ok'].
+        **pdplotkwargs
+            Additional keyword arguments passed to pandas.Series.plot().
+
+        Returns
+        -------
+        matplotlib.axes.Axes
+            The axes object containing the plot.
+
+        Notes
+        -----
+        The plot excludes records that don't match the specified labels by 
+        converting them to NaN rather than subsetting, which prevents unwanted 
+        interpolation between valid data points.
+        
+        Notes
+        -------
+        Tip: You can inspect all the present labels (and their counts) by using: `sensordata.df['label'].value_counts()`.
+        
+        """
+        
+        df = self.df
+        #filter by label (do not subset, but convert others to Nan to avoid interpolation)
+        df.loc[~df['label'].isin(show_labels), 'value'] = np.nan
+        
+        #convert to pandas series
+        df.index = df.index.droplevel('obstype') #drop obstype level
+        series = df['value']
+        series.name = f'{self.stationname}:{self.obstype.name}'
+        #make plot
+        axs = series.plot(**pdplotkwargs)
+        
+        return axs
+        
 
     # ------------------------------------------
     #    Quality Control (technical qc + value-based qc)
