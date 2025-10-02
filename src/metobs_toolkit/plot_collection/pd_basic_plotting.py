f78a7028125daf3154d901246f5fe482dfc4e04b
Thomas Vergauwen <thomas.vergauwen@meteo.be>
2025-10-02 10:41:54 +0200

    code review fixes

--- src/metobs_toolkit/plot_collection/pd_basic_plotting.py
+++ src/metobs_toolkit/plot_collection/pd_basic_plotting.py
@@ -2,8 +2,7 @@ import numpy as np
 from matplotlib.pyplot import Axes
 from metobs_toolkit.backend_collection.loggingmodule import log_entry
 
-
-
+@log_entry
 def modeldata_simple_pd_plot(modeltimeseries, **pdplotkwargs) -> Axes:
     """
     A wrapper on the pandas.series.plot function for ModelTimeSeries.
@@ -17,23 +16,25 @@ def modeldata_simple_pd_plot(modeltimeseries, **pdplotkwargs) -> Axes:
     -------
     matplotlib.axes.Axes
         The axes object containing the plot.
-        
+
     """
-    #set name (so that labels match the id)
+    # set name (so that labels match the id)
     plot_series = modeltimeseries.series
     plot_series.name = modeltimeseries._id()
     return plot_series.plot(**pdplotkwargs)
 
 
 @log_entry
-def sensordata_simple_pd_plot(sensordata, show_labels: list=['ok'], **pdplotkwargs) -> Axes:
+def sensordata_simple_pd_plot(
+    sensordata, show_labels: list = ["ok"], **pdplotkwargs
+) -> Axes:
     """
     A wrapper on the pandas.series.plot function for SensorData.
 
     Parameters
     ----------
     show_labels : list of str, optional
-        List of quality control labels to include in the plot. Records with 
+        List of quality control labels to include in the plot. Records with
         other labels are converted to NaN to avoid interpolation artifacts.
         Default is ['ok'].
     **pdplotkwargs
@@ -46,25 +47,23 @@ def sensordata_simple_pd_plot(sensordata, show_labels: list=['ok'], **pdplotkwar
 
     Notes
     -----
-    The plot excludes records that don't match the specified labels by 
-    converting them to NaN rather than subsetting, which prevents unwanted 
-    interpolation between valid data points.
-    
-    Notes
-    -------
-    Tip: You can inspect all the present labels (and their counts) by using: `sensordata.df['label'].value_counts()`.
-    
+    * Tip: You can inspect all the present labels (and their counts) by using: `sensordata.df['label'].value_counts()`.
+
+    * The plot excludes records that don't match the specified labels by
+     converting them to NaN rather than subsetting, which prevents unwanted
+     interpolation between valid data points.
+     
     """
-    
+
     df = sensordata.df
-    #filter by label (do not subset, but convert others to Nan to avoid interpolation)
-    df.loc[~df['label'].isin(show_labels), 'value'] = np.nan
-    
-    #convert to pandas series
-    df.index = df.index.droplevel('obstype') #drop obstype level
-    series = df['value']
+    # filter by label (do not subset, but convert others to Nan to avoid interpolation)
+    df.loc[~df["label"].isin(show_labels), "value"] = np.nan
+
+    # convert to pandas series
+    df.index = df.index.droplevel("obstype")  # drop obstype level
+    series = df["value"]
     series.name = sensordata._id()
-    #make plot
+    # make plot
     axs = series.plot(**pdplotkwargs)
-    
+
     return axs
