0b7021a3de051f3989e40ed3cfea4cb8d9111f48
Thomas Vergauwen <thomas.vergauwen@meteo.be>
2026-01-14 13:24:46 +0100

    new futur proof structure of saving and comparing solutions in testpipeline

--- tests/solutionclass.py
+++ tests/solutionclass.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import pickle
 
 import pandas as pd
+import geopandas as gpd
+import json
 import pandas.testing
 
 libfolder = Path(str(Path(__file__).resolve())).parent.parent
@@ -9,57 +11,203 @@ libfolder = Path(str(Path(__file__).resolve())).parent.parent
 datadir = libfolder.joinpath("tests").joinpath("data")
 
 
-class SolutionFixer:
-    def __init__(self, solutiondir):
-        self.solutiondir = Path(solutiondir)
+# class SolutionFixer:
+#     def __init__(self, solutiondir):
+#         self.solutiondir = Path(solutiondir)
 
-    def get_solution(self, testfile, classname, methodname):
-        basefoldername = f"{str(testfile).strip('.py')}_solutions"
-        subfoldername = f"{classname.lower()}"
-        trgfilename = f"{str(methodname)}.pkl"
+#     def get_solution(self, testfile, classname, methodname):
+#         basefoldername = f"{str(testfile).strip('.py')}_solutions"
+#         subfoldername = f"{classname.lower()}"
+#         trgfilename = f"{str(methodname)}.pkl"
 
-        solutionfile = (
-            self.solutiondir.joinpath(basefoldername)
-            .joinpath(subfoldername)
-            .joinpath(trgfilename)
+#         solutionfile = (
+#             self.solutiondir.joinpath(basefoldername)
+#             .joinpath(subfoldername)
+#             .joinpath(trgfilename)
+#         )
+#         if not solutionfile.exists():
+#             raise SolutionNotExisting(
+#                 f"The solution of {testfile} -->{classname}:{methodname} does not exist (at {solutionfile})!"
+#             )
+
+#         with open(solutionfile, "rb") as file:
+#             solution_data = pickle.load(file)
+#         return solution_data
+
+#     def create_solution(self, solutiondata, testfile, classname, methodname):
+#         # construct path
+#         basefoldername = f"{str(testfile).strip('.py')}_solutions"
+#         subfoldername = f"{classname.lower()}"
+#         trgfilename = f"{str(methodname)}.pkl"
+
+#         solutionfile = (
+#             self.solutiondir.joinpath(basefoldername)
+#             .joinpath(subfoldername)
+#             .joinpath(trgfilename)
+#         )
+#         # clear previous solutions
+#         if solutionfile.exists():
+#             print(
+#                 f"!! OVERWRITING SOLUTION FOR  {testfile} --> {classname}:{methodname} !!! "
+#             )
+#             # delete file
+#             solutionfile.unlink()
+
+#         # Create directory if it does not exist
+#         solutionfile.parent.mkdir(parents=True, exist_ok=True)
+
+#         # Pickle data object
+#         with open(solutionfile, "wb") as file:
+#             pickle.dump(solutiondata, file)
+
+
+class SolutionFixer2:
+    """Store solutions as data files, not pickled objects."""
+    
+    def __init__(self, solutiondir: Path):
+        self.solutiondir = solutiondir
+        
+    
+    def get_solution_dir(self, methodname: str, testfile: str, classname: str) -> Path:
+
+        solutiondir = (
+            self.solutiondir.joinpath(f"{str(testfile).strip('.py')}_solutions")
+            .joinpath(f"{classname.lower()}")
+            .joinpath(f"{methodname}")
         )
-        if not solutionfile.exists():
+        
+        if not solutiondir.exists():
+            solutiondir.mkdir(parents=True, exist_ok=True)
+        return solutiondir
+    
+    def create_solution(self, solution, methodname: str, testfile: str, classname: str):
+        """Extract and save only the data attributes."""
+        base_path = self.get_solution_dir(methodname, testfile, classname)
+        base_path.mkdir(exist_ok=True)
+        
+       
+        print(
+            f"!! OVERWRITING SOLUTION FOR  {testfile} --> {classname}:{methodname} !!! "
+        )
+        #if solution is Metobs-toolkit.Dataset
+        if solution.__class__.__name__ == "Dataset":
+            store_dataset(solution, base_path)
+            
+        elif solution.__class__.__name__ == "Station":
+            store_station(solution, base_path)
+        
+        elif isinstance(solution, pd.DataFrame):
+            store_pandasdf(solution, base_path)
+        else:
+            raise NotImplementedError(
+                "SolutionFixer2.create_solution only supports Dataset, Station, and Analysis objects.")
+        
+           
+    def get_solution(self, methodname: str, testfile: str, classname: str) -> dict:
+        """Load solution data as a dict of DataFrames."""
+        base_path = self.get_solution_dir(methodname, testfile, classname)
+        if not base_path.exists():
             raise SolutionNotExisting(
-                f"The solution of {testfile} -->{classname}:{methodname} does not exist (at {solutionfile})!"
+                f"The solution at {base_path} does not exist!"
             )
 
-        with open(solutionfile, "rb") as file:
-            solution_data = pickle.load(file)
-        return solution_data
+        with open(base_path / "datatype.json", "r") as f:
+            metobsobj = json.load(f)
+        
+        solutionclass = metobsobj.get("class")
+        if solutionclass in ['Dataset', 'Station']:
+            def file_to_attr(fpath: Path) -> str:
+                return fpath.stem.replace("solution_", "")
 
-    def create_solution(self, solutiondata, testfile, classname, methodname):
-        # construct path
-        basefoldername = f"{str(testfile).strip('.py')}_solutions"
-        subfoldername = f"{classname.lower()}"
-        trgfilename = f"{str(methodname)}.pkl"
+            result = {}
+            for f in base_path.glob("solution_*.parquet"):
+                result[file_to_attr(f)] = pd.read_parquet(f)
+            
+            if solutionclass == 'Dataset':
+                return SerializedDataset(result)
+            else:
+                return SerializedStation(result)
+         
+        elif solutionclass == 'DataFrame':
+            df = pd.read_parquet(base_path / "solution_df.parquet")
+            return df
+            
+        else:
+            raise NotImplementedError(
+                "SolutionFixer2.get_solution only supports Dataset, Station, and DataFrame objects.")
+            
+        
+        
+def store_dataset(dataset, dir: Path):
+    """Store the dataset as a json-serializable dict."""
+    
+    dir.mkdir(parents=True, exist_ok=True)
+    with open(dir / "datatype.json", "w") as f:
+            json.dump({"class": "Dataset"}, f)
+    dataset.df.to_parquet(dir / "solution_df.parquet")
+    dataset.metadf.to_parquet(dir / "solution_metadf.parquet")
+    dataset.gapsdf.to_parquet(dir / "solution_gapsdf.parquet")
+    dataset.modeldatadf.to_parquet(dir / "solution_modeldatadf.parquet")
+    dataset.outliersdf.to_parquet(dir / "solution_outliersdf.parquet")
+    
+    #Specific attributes
+    # solution.obstypes.to_parquet(dir / "solution_obstypes.parquet")
+    
+def store_station(station, dir: Path):
+    """Store the dataset as a json-serializable dict."""
+    
+    dir.mkdir(parents=True, exist_ok=True)
+    with open(dir / "datatype.json", "w") as f:
+            json.dump({"class": "Station"}, f)
+    station.df.to_parquet(dir / "solution_df.parquet")
+    station.metadf.to_parquet(dir / "solution_metadf.parquet")
+    station.gapsdf.to_parquet(dir / "solution_gapsdf.parquet")
+    station.modeldatadf.to_parquet(dir / "solution_modeldatadf.parquet")
+    station.outliersdf.to_parquet(dir / "solution_outliersdf.parquet")
 
-        solutionfile = (
-            self.solutiondir.joinpath(basefoldername)
-            .joinpath(subfoldername)
-            .joinpath(trgfilename)
-        )
-        # clear previous solutions
-        if solutionfile.exists():
-            print(
-                f"!! OVERWRITING SOLUTION FOR  {testfile} --> {classname}:{methodname} !!! "
-            )
-            # delete file
-            solutionfile.unlink()
+def store_pandasdf(df: pd.DataFrame, dir: Path):
+    """Store the dataset as a json-serializable dict."""
+    
+    dir.mkdir(parents=True, exist_ok=True)
+    with open(dir / "datatype.json", "w") as f:
+            json.dump({"class": "DataFrame"}, f)
+    df.to_parquet(dir / "solution_df.parquet")
+    
+class SerializedDataset():
+    """A class to hold the deserialized data of a Dataset for comparison."""
+    
+    def __init__(self, data: dict):
+        self.df = data.get("df")
+        self.metadf = format_metadata(data.get("metadf"))
+        self.gapsdf = data.get("gapsdf")
+        self.modeldatadf = data.get("modeldatadf")
+        self.outliersdf = data.get("outliersdf")
+        
+        
+class SerializedStation():
+    """A class to hold the deserialized data of a Dataset for comparison."""
+    
+    def __init__(self, data: dict):
+        self.df = data.get("df")
+        self.metadf = format_metadata(data.get("metadf"))
+        self.gapsdf = data.get("gapsdf")
+        self.modeldatadf = data.get("modeldatadf")
+        self.outliersdf = data.get("outliersdf")
+       
+def format_metadata(metadf):
+    
+    #geoseries not serializable to json, so recreate
+    if 'geometry' in metadf.columns:
+        metadf = gpd.GeoDataFrame(metadf, geometry=gpd.points_from_xy(metadf['lon'],
+                                                                      metadf['lat']))
+    return metadf
 
-        # Create directory if it does not exist
-        solutionfile.parent.mkdir(parents=True, exist_ok=True)
-
-        # Pickle data object
-        with open(solutionfile, "wb") as file:
-            pickle.dump(solutiondata, file)
 
 
-def assert_equality(to_check, solution):
+
+
+
+def assert_equality(to_check, solution, **kwargs):
     """Returns some debug help when the to_check is not equalt to the solution
 
     This output is used for developping to point in more details why a test
@@ -70,8 +218,18 @@ def assert_equality(to_check, solution):
 
     """
 
+    if ((to_check.__class__.__name__ == "Dataset") and 
+        (solution.__class__.__name__ == "SerializedDataset")):
+        seriealize_comparison(to_check, solution, **kwargs)
+        
+    
+    elif ((to_check.__class__.__name__ == "Station") and 
+        (solution.__class__.__name__ == "SerializedStation")):
+        seriealize_comparison(to_check, solution, **kwargs)
+         
+    
     # type equal test
-    if type(to_check) != type(solution):
+    elif type(to_check) != type(solution):
         retstr = f"DIFF: to_check type is {type(to_check)}, while solution is of type {type(solution)} "
         raise AssertionError(retstr)
 
@@ -133,6 +291,7 @@ def assert_equality(to_check, solution):
             right=solution,
             check_exact=False,
             rtol=0.001,
+            
         )
 
     # pandas dataframes
@@ -144,28 +303,28 @@ def assert_equality(to_check, solution):
             rtol=0.001,
         )
 
-    # metobs_toolkit.Dataset test
-    elif to_check.__class__.__name__ == "Dataset":
-        compare_df_attr(to_check, solution, "metadf")
-        compare_df_attr(to_check, solution, "gapsdf")
-        compare_df_attr(to_check, solution, "modeldatadf")
-        compare_df_attr(to_check, solution, "outliersdf", exclude_columns="details")
-        compare_df_attr(to_check, solution, "df")
-        assert (
-            to_check.obstypes == solution.obstypes
-        ), "There is a mismatch in obstypes with the solution!"
+    # # metobs_toolkit.Dataset test
+    # elif to_check.__class__.__name__ == "Dataset":
+    #     compare_df_attr(to_check, solution, "metadf")
+    #     compare_df_attr(to_check, solution, "gapsdf")
+    #     compare_df_attr(to_check, solution, "modeldatadf")
+    #     compare_df_attr(to_check, solution, "outliersdf", exclude_columns="details")
+    #     compare_df_attr(to_check, solution, "df")
+    #     assert (
+    #         to_check.obstypes == solution.obstypes
+    #     ), "There is a mismatch in obstypes with the solution!"
 
-    # metobs_toolkit.Station test
-    elif to_check.__class__.__name__ == "Station":
-        compare_df_attr(to_check, solution, "metadf")
-        compare_df_attr(to_check, solution, "gapsdf")
-        compare_df_attr(to_check, solution, "modeldatadf")
-        compare_df_attr(to_check, solution, "outliersdf", exclude_columns="details")
-        compare_df_attr(to_check, solution, "df")
-    # metobs_toolkit.Station test
-    elif to_check.__class__.__name__ == "Analysis":
-        compare_df_attr(to_check, solution, "metadf")
-        compare_df_attr(to_check, solution, "df")
+    # # metobs_toolkit.Station test
+    # elif to_check.__class__.__name__ == "Station":
+    #     compare_df_attr(to_check, solution, "metadf")
+    #     compare_df_attr(to_check, solution, "gapsdf")
+    #     compare_df_attr(to_check, solution, "modeldatadf")
+    #     compare_df_attr(to_check, solution, "outliersdf", exclude_columns="details")
+    #     compare_df_attr(to_check, solution, "df")
+    # # metobs_toolkit.Station test
+    # elif to_check.__class__.__name__ == "Analysis":
+    #     compare_df_attr(to_check, solution, "metadf")
+    #     compare_df_attr(to_check, solution, "df")
 
     # Else
     else:
@@ -196,6 +355,15 @@ def compare_df_attr(testobj, solutionobj, attr, exclude_columns=None):
         )
     except AssertionError as e:
         raise AssertionError(f"DIFF in {attr}-attribute:\n " + str(e))
+
+def seriealize_comparison(to_check, solution, **kwargs):
+    solutiondict = solution.__dict__
+    for key in solutiondict.keys():
+        compare_df_attr(testobj=to_check, solutionobj=solution, attr=key, **kwargs)
+        
+    
+    
+
 
 
 class UnforseenDifference(Exception):
